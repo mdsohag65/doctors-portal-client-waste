37d83a326b7887696d07699d5769c7502e0ec654
mdsohag65 <sohag92ahmed@gmail.com>
2022-05-10 13:17:38 +0600

    setting the infoCard

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Banner from './Banner';
+import Info from './Info';
+
 
 const Home = () => {
     return (
         <div>
-            <h2>this is home</h2>
+            <Banner></Banner>
+            <Info></Info>
         </div>
     );
 };
