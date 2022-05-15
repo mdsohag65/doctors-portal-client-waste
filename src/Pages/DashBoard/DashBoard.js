dea495de6767c4b041fac86f78363688740cff29
mdsohag65 <sohag92ahmed@gmail.com>
2022-05-15 20:24:22 +0600

    dashboard done

--- src/Pages/DashBoard/DashBoard.js
+++ src/Pages/DashBoard/DashBoard.js
@@ -4,7 +4,7 @@ import { Link, Outlet } from 'react-router-dom';
 const DashBoard = () => {
     return (
         <div class="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+            <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
             <div class="drawer-content">
                 <h2 className='text-3xl font-bold text-purple-400'>Your DashBoard</h2>
                 <Outlet></Outlet>
@@ -12,7 +12,7 @@ const DashBoard = () => {
 
             </div>
             <div class="drawer-side">
-                <label for="my-drawer-2" class="drawer-overlay"></label>
+                <label for="dashboard-sidebar" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
                     <li><Link to="/dashboard">Sidebar Item 1</Link></li>
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -43,7 +43,9 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open DashBoard</label>
+                    <label tabIndex="1" for="dashboard-sidebar" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
                 </div>
             </div>
 
